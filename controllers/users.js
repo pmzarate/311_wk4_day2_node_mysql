d867974543fb13c369856d02664fb1e86506a339
pmzarate <patriciamarakzarate@yahoo.com>
2019-09-26 18:01:02 -0500

    finalize pmzarate

--- controllers/users.js
+++ controllers/users.js
@@ -51,7 +51,7 @@ const deleteUserByFirstName = (req, res) => {
   // DELETE FROM USERS WHERE FIRST NAME = <REQ PARAMS FIRST_NAME>
   let sql = "DELETE FROM users WHERE first_name = (req.param.first_name)"
   // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, [])
+  sql = mysql.format(sql, [req.body.first_name])
 
   pool.query(sql, (err, results) => {
     if (err) return handleSQLError(res, err)
